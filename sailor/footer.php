a2e75c88f2a137105650f69cd1f607a02059d076
Ross MacPhee <officialsalient@gmail.com>
2016-04-03 21:25:46 +0100

    Added Theme to repo
    Added all the theme's files to the repo.

---
+++ sailor/footer.php
@@ -0,0 +1,16 @@
+			<div id="Footer">
+				<div class="wrap">
+		            <footer id="bottom">
+		                <small><b>&copy; <?php echo date('Y'); ?></b>. Seems like you've reached the end&hellip;</small>
+
+		                <ul role="navigation">
+		                    <li><a href="<?php echo rss_url(); ?>">Feed Me</a></li>
+		                    <li><a href="<?php echo base_url('feeds/json'); ?>">Bleep Bloop</a></li>
+		                </ul>
+		            </footer>
+
+		        </div>
+			</div>
+        </div>
+    </body>
+</html>
---
+++ sailor/header.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+	<head prefix="og: http://ogp.me/ns#">
+
+		<!-- Header Configuration -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Article / Page's Title -->
+		<title><?php echo site_name(); ?> - <?php echo page_title('Page can’t be found'); ?></title>
+
+		<!-- Article / Page's Description and Generator -->
+		<meta name="description" content="<?php echo site_description(); ?>">
+		<meta name="generator" content="Anchor CMS">
+
+		<!-- Website's stylesheets -->
+		<link rel="stylesheet" href="<?php echo theme_url('/css/reset.css'); ?>" media="none" onload="if(media!='all')media='all'">
+		<link rel="stylesheet" href="<?php echo theme_url('/css/style.css'); ?>" media="none" onload="if(media!='all')media='all'">
+		<link rel="stylesheet" href="<?php echo theme_url('/css/small.css'); ?>" media="none" onload="if(media!='all')media='(max-width: 830px)'">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300" media="none" onload="if(media!='all')media='all'">
+
+		<!-- Link to RSS feed and Icon -->
+		<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php echo rss_url(); ?>">
+		<link rel="shortcut icon" href="<?php echo theme_url('img/favicon.png'); ?>">
+
+		<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Website's scripts -->
+		<script>var base = '<?php echo theme_url(); ?>';</script>
+		<script src="<?php echo asset_url('/js/zepto.js'); ?>"></script>
+		<script src="<?php echo theme_url('/js/main.js'); ?>"></script>
+
+		<!-- Objectify website's content -->
+	    <meta property="og:title" content="<?php echo site_name(); ?>">
+	    <meta property="og:type" content="WebSite">
+	    <meta property="og:url" content="<?php echo e(current_url()); ?>">
+	    <meta property="og:image" content="<?php echo theme_url('img/og_image.gif'); ?>">
+	    <meta property="og:site_name" content="<?php echo site_name(); ?>">
+	    <meta property="og:description" content="<?php echo site_description(); ?>">
+
+		<?php if(customised()): ?>
+		    <!-- Custom CSS -->
+    		<style><?php echo article_css(); ?></style>
+
+    		<!--  Custom Javascript -->
+    		<script><?php echo article_js(); ?></script>
+		<?php endif; ?>
+	</head>
+	<body class="<?php echo body_class(); ?>">
+		<div class="main-wrap">
+			<header id="top">
+				<div class="wrap">
+					<a href="<?php echo base_url(); ?>" title="<?php echo site_name(); ?>"><img id="logo" src="<?php echo theme_url('/img/logo.png'); ?>" alt="<?php echo site_name(); ?>" /></a>
+
+					<div id="navLinks">
+						<ul>
+							<?php if(has_menu_items()):
+								while(menu_items()): ?>
+							<li <?php echo (menu_active() ? 'class="active"' : ''); ?>>
+								<a href="<?php echo menu_url(); ?>" title="<?php echo menu_title(); ?>">
+									<?php echo menu_name(); ?>
+								</a>
+							</li>
+							<?php endwhile;
+								endif; ?>
+						</ul>
+					</div>
+					<form id="search" action="<?php echo search_url(); ?>" method="post">
+						<input type="search" id="term" name="term" placeholder="Search" value="<?php echo search_term(); ?>">
+						<input type="hidden" id="whatSearch" name="whatSearch" value="all" />
+					</form>
+					<div id="headliner">
+						<h1><?php echo page_title('Page can’t be found'); ?></h1>
+					</div>
+				</div>
+			</header>
