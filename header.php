4ee3c82fe04f2852e2e43903b453e1622f732c2d
Ross MacPhee <SirRossMacPhee@Ross.home>
2016-04-04 02:46:45 +0100

    No more render-blocking JS
    Added an async JS function to load all javascript files in async and in
    order as not to stop the order-of-operations.

--- header.php
+++ header.php
@@ -39,8 +39,6 @@
 
 		<!-- Website's scripts -->
 		<script>var base = '<?php echo theme_url(); ?>';</script>
-		<script src="<?php echo asset_url('/js/zepto.js'); ?>"></script>
-		<script src="<?php echo theme_url('/js/main.js'); ?>"></script>
 
 		<!-- Objectify website's content -->
 	    <meta property="og:title" content="<?php echo site_name(); ?>">
